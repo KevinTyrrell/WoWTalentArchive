182adc9ac93fb98ffadb32d0fbcc8db5cb44d8a0
Kevin Tyrrell <kev070892@gmail.com>
2018-05-29 23:20:43 -0700

    Completely re-worked inheritance. * Protected members defined in sub-classes are no longer accessible from parent classes. * Protected and private members of a class are now accessible from other instances of that class.

--- src/scripts/lib/Structure.js
+++ src/scripts/lib/Structure.js
@@ -6,91 +6,48 @@
  */
 const Structure = (function()
 {
-    /* Module design pattern. */
-    const module = { };
-
-    /* Protected constructor. */
-    const constructor = (function()
+    const extend = function(module, superConstructor)
     {
-        /* Associates JSON object's in the Structure module as Structure instances. */
-        const associate = InstanceManager.manage(module);
-
-        return function()
-        {
-            /* Structure instance. */
-            const instance = { };
-            associate(instance);
-
-            /* ~~~~~~~~~~ Public member(s) ~~~~~~~~~~ */
-
-            /**
-             * Converts a Structure to a String representation.
-             * This function should be overridden in subclasses.
-             * Not to be confused with prototype.toString.
-             * @returns {string} String representation.
-             */
-            instance.tostring = function()
-            {
-                return "structure";
-            };
-
-            return instance;
-        }
-    })();
-
-    /**
-     * Public constructor.
-     * @returns {ReadonlyArray<{}>} Structure instance.
-     */
-    module.new = function()
-    {
-        return Object.freeze(constructor());
-    };
-
-    /**
-     * Facilitates primitive sub-classing of the Structure class.
-     * Sub-classes will have access to mutable objects and protected object members.
-     * @returns {Readonly<{new: new, getProtected: getProtected}>} Protected super-constructor & member(s).
-     */
-    module.extend = function()
-    {
-        /* Protected members of each instance for this module (and all of its extended modules). */
         const protectedMap = new WeakMap();
+        const privateMap = new WeakMap();
+
+        const hasInstance = function(instance)
+        {
+            privateMap.has(instance);
+        };
+        module.hasInstance = hasInstance;
+
+        const constructorHook = function()
+        {
+            const obj = superConstructor();
+            const instance = obj.instance;
+            const prot = { super: obj.protected };
+            protectedMap.set(instance, prot);
+            privateMap.set(instance, { });
+
+            return Object.freeze({
+                instance: instance,
+                protected: prot
+            });
+        };
 
         return Object.freeze({
-            /**
-             * Protected constructor.
-             * @returns {*} Structure object.
-             */
-            new: function()
-            {
-                const instance = constructor();
-                /* The structure's protected member(s). */
-                const restricted = { };
-                /* Pair each instance with its protected member(s). */
-                protectedMap.set(instance, restricted);
-                return instance;
-            },
-
-            /**
-             * Provides a primitive way for sub-classes to access instance's protected members.
-             * Note that placing identifiers into the returned object from a sub-class
-             * allows for super-classes to access those protected members.
-             * To fix that behavior may involve having separate protected members for
-             * each extending class, but still interconnecting them in a one-way relationship.
-             * @param instance Instance to inspect.
-             * @returns {{}} Protected members of the instance.
-             */
-            getProtected: function(instance)
+            new: constructorHook,
+            protected: function(instance)
             {
                 assert(Boolean(instance));
                 assert(typeof instance === "object");
-                assert(module.hasInstance(instance));
-                assert(protectedMap.has(instance));
+                assert(hasInstance(instance));
                 return protectedMap.get(instance);
-            }
+            },
+            private: function(instance)
+            {
+                assert(Boolean(instance));
+                assert(typeof instance === "object");
+                assert(hasInstance(instance));
+                return privateMap.get(instance);
+            },
+            extend: extend
         });
     };
-
-    return Object.freeze(module);
 })();
